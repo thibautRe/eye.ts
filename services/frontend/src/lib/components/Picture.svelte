d7a5f17ac5924e7bce53542fd9a4ef9c4104a0d3
Thibaut Remy <thibaut@thibaut.re>
2025-05-05 21:30:25 +0200

    feat: picture grid horiontal massonry

--- services/frontend/src/lib/components/Picture.svelte
+++ services/frontend/src/lib/components/Picture.svelte
@@ -39,6 +39,6 @@
     position: relative;
     height: 100%;
     width: 100%;
-    object-fit: contain;
+    object-fit: cover;
   }
 </style>
--- services/frontend/src/lib/components/PictureGrid.svelte
+++ services/frontend/src/lib/components/PictureGrid.svelte
@@ -2,24 +2,36 @@
   import type { PictureApi } from "api-types"
   import Picture from "./Picture.svelte"
   import { makePictureUrl } from "$lib/urls"
+  import { splitInLines } from "$lib/splitInLines"
 
   const { pics }: { pics: PictureApi[] } = $props()
+  const lines = $derived(splitInLines(pics))
 </script>
 
 <div class="wrapper">
-  {#each pics as pic}
-    <a class="link" href={makePictureUrl(pic.id)}>
-      <Picture {pic} sizes="23vw" />
-    </a>
+  {#each lines as line}
+    <div class="line" style="aspect-ratio: {line.aspectRatio};">
+      {#each line.pictures as pic}
+        <a class="link" href={makePictureUrl(pic.id)}>
+          <Picture {pic} sizes="23vw" />
+        </a>
+      {/each}
+    </div>
   {/each}
 </div>
 
 <style>
   .wrapper {
     display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    gap: 8px;
+    flex-direction: column;
+
+    --gap: 8px;
+    gap: var(--gap);
+  }
+
+  .line {
+    display: flex;
+    gap: var(--gap);
   }
 
   .link {
