56a82ce607af8023990563d371aab4be1ab76a93
Thibaut Remy <thibaut@thibaut.re>
2025-05-12 21:59:03 +0200

    chore: drop svelte

--- services/frontend/src/routes/categories/[slug]/edit/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { CategoryApi } from "api-types"
-import { slugify } from "core"
-import type { PageLoad } from "./$types"
-import { apiGetCategory } from "$lib/api"
-
-export const load: PageLoad = async ({ params }): Promise<CategoryApi> =>
-  await apiGetCategory(slugify(params.slug))
