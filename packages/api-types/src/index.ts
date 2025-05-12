fcb84f8d5a2013a23aa587b38ff5a18cf513857f
Thibaut Remy <thibaut@thibaut.re>
2025-05-12 21:34:49 +0200

    chore: formatting

--- packages/api-types/src/index.ts
+++ packages/api-types/src/index.ts
@@ -83,31 +83,17 @@ export type ApiRouteKey = ApiRoutes["key"]
 export type ApiRoute<K extends ApiRouteKey> = Extract<ApiRoutes, { key: K }>
 
 export type ApiRouteArgs<K extends ApiRouteKey> =
-  ApiRoute<K> extends {
-    args: infer A
-  }
-    ? A
-    : null
+  ApiRoute<K> extends { args: infer A } ? A : null
 export type ApiRouteResponse<K extends ApiRouteKey> =
-  ApiRoute<K> extends {
-    response: infer R
-  }
-    ? R
-    : null
+  ApiRoute<K> extends { response: infer R } ? R : null
 export type ApiRouteSearchParams<K extends ApiRouteKey> =
-  ApiRoute<K> extends {
-    searchParams: infer S
-  }
+  ApiRoute<K> extends { searchParams: infer S }
     ? S extends string
       ? S
       : null
     : null
 export type ApiRouteJson<K extends ApiRouteKey> =
-  ApiRoute<K> extends {
-    json: infer J
-  }
-    ? J
-    : never
+  ApiRoute<K> extends { json: infer J } ? J : never
 
 export interface ApiPathnameWithArgs<A> {
   method: ApiMethod
